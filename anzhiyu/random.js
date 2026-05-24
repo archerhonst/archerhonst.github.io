85705e3981848240489caf094d1ae04c9e732b2f
archerhonst <wangyiguang0322@163.com>
2026-05-24 18:56:31 +0800

    Site updated: 2026-05-24 18:56:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/05/24/hello-world/"];function toRandomPost(){
+var posts=["undefined/3eeb.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
